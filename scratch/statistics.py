b6aacb5712b3212fcd3889df3fca6f990a9d9004
jrkd5 <jaroslaw.dulinski@gmail.com>
2022-01-28 17:51:54 +0100

    Added function: correlation

--- scratch/statistics.py
+++ scratch/statistics.py
@@ -75,3 +75,13 @@ def interquantile_range(xs: List[float]) -> float:
 def covariance(xs: List[float], ys: List[float]) -> float:
     assert len(xs) == len(ys), "x and y must be the same length"
     return dot(de_mean(xs), de_mean(ys)) / (len(xs) - 1)
+
+
+def correlation(xs: List[float], ys: List[float]) -> float:
+    """Measures how much xs and ys vary in tandem about their means"""
+    stdev_x = standard_deviation(xs)
+    stdev_y = standard_deviation(ys)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(xs, ys) / stdev_x / stdev_y
+    else:
+        return 0  # if no variation, correlation is 0
